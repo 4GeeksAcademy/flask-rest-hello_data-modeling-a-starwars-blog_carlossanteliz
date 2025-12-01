e17b0a12f8eb44d33dd142b35f3bce1c112b634c
Charliekofg <charliekofg@hotmail.com>
2025-12-01 22:00:27 +0000

    tbd:continue with FavoriteCharacter

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
@@ -9,6 +10,8 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    planets: Mapped[List["FavoritePlanet"]] = relationship()
+    characters: Mapped[List["FavoritePeople"]] = relationship()
 
 
     def serialize(self):
@@ -18,11 +21,34 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
 
-class Character(db.Model):
+class Character(db.Model): 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
-    gender: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(40), nullable=False)
     hair_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    species: Mapped[str] = mapped_column(String(40), nullable=False)
+    character: Mapped[List["FavoritePeople"]] = relationship(back_populates="People")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "species": self.species,
+            "charachter": self.character
+        }
+    
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    climate: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    rotation_period: Mapped[int] = mapped_column(Integer, unique=False, nullable=False)
+    gravity: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
+    terrain: Mapped[str] = mapped_column(String(40), unique=True, nullable=False)
 
 
-class FavoriteCharacter
+class FavoriteCharacter(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    id_people: Mapped[int] = mapped_column(ForeignKey("people.id"), nullable=True)
+    user_id: Mapped
